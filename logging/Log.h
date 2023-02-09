bcecd8a2bb36f30cd912e7f4f2ca084713334a08
Stephen Foulds <stephen.foulds@consult.red>
2023-02-09 11:22:10 +0000

    Simple logging, add cmake template

---
+++ logging/Log.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string.h>
+
+#ifndef __FILENAME__
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#endif
+
+#define Log(fmt, ...)                                                                                   \
+    do {                                                                                                \
+        fprintf(stderr, "[%s:%d](%s): " fmt "\n", __FILENAME__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+        fflush(stderr);                                                                                 \
+    } while (0)
