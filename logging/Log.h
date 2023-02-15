c6ba5f1529fdbde58f835d4da09519c12a1aa552
Stephen Foulds <stephen.foulds@consult.red>
2023-02-15 14:08:16 +0000

    Use built-in gcc file_name macro if available

--- logging/Log.h
+++ logging/Log.h
@@ -4,12 +4,12 @@
 #include <cstdio>
 #include <cstring>
 
-#ifndef __FILENAME__
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#ifndef __FILE_NAME__
+#define __FILE_NAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #endif
 
 #define Log(fmt, ...)                                                                                   \
     do {                                                                                                \
-        fprintf(stderr, "[%s:%d](%s): " fmt "\n", __FILENAME__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+        fprintf(stderr, "[%s:%d](%s): " fmt "\n", __FILE_NAME__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
         fflush(stderr);                                                                                 \
     } while (0)
